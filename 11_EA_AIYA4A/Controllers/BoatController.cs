aeb6b67aac7a5d1c6f3274c601684c69b90cb967
Amateka <Amateka@LAPTOP-HJLKKQ4E>
2022-11-30 11:49:33 +0100

    Add project files.

---
+++ 11_EA_AIYA4A/Controllers/BoatController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _11_EA_AIYA4A.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("hajo/kerdesek")]
+        public IActionResult függvénynév()
+        {
+            Models.HajosContext hajosContext = new Models.HajosContext();
+            var lisa = from x in hajosContext.Questions select x;
+            return Ok(lisa);
+        }
+        [HttpGet]
+        [Route("hajo/kerdesek/{id}")]
+        public IActionResult f2(int id)
+        {
+            Models.HajosContext hajosContext = new Models.HajosContext();
+            var lisa = from x in hajosContext.Questions
+                       where x.QuestionId == id
+                       select x;
+            var lisa2 = hajosContext.Questions.Where( x => x.QuestionId == id );
+
+            return Ok(lisa.FirstOrDefault());
+        }
+    }
+}
